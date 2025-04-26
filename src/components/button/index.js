e911708dcd3c9deed3923f1b5cb19c59e31911e6
DlopezS98 <01dlopezs98@gmail.com>
2025-04-25 23:01:35 -0600

    Update Button component to allow custom button type via props

--- src/components/button/index.js
+++ src/components/button/index.js
@@ -2,7 +2,7 @@ import './button.css';
 
 export default function Button(props) {
   return (
-    <button type="button" className={`isi-button ${props.variant}`} disabled={props.disabled} onClick={props.onClick}>
+    <button type={props.type || "button"} className={`isi-button ${props.variant}`} disabled={props.disabled} onClick={props.onClick}>
       {props.children}
     </button>
   );
